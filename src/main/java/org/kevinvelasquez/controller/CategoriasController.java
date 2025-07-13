c483f1d0f2ab3959bf102a141dab8f2d1df967bc
Compkev22 <kvelasquez-2022473@kinal.edu.gt>
2025-07-12 20:53:57 -0600

    Commit Seis: Se construyo un poco el controlador de Categorias

--- src/main/java/org/kevinvelasquez/controller/CategoriasController.java
+++ src/main/java/org/kevinvelasquez/controller/CategoriasController.java
@@ -4,10 +4,35 @@
  */
 package org.kevinvelasquez.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import org.kevinvelasquez.system.Main;
+
 /**
  *
  * @author Kevin
  */
-public class CategoriasController {
+public class CategoriasController implements Initializable {
+    private Main principal;
+
+    public CategoriasController() {
+    }
+
+    public CategoriasController(Main principal) {
+        this.principal = principal;
+    }
+
+    public Main getPrincipal() {
+        return principal;
+    }
+
+    public void setPrincipal(Main principal) {
+        this.principal = principal;
+    }
     
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }      
 }
