6c928ec4d8c95691ce63f1a67a757085267693d0
Compkev22 <kvelasquez-2022473@kinal.edu.gt>
2025-07-14 23:43:14 -0600

    Commit Cuarenta y Nueve: Se modifico el controller de Inicio

--- src/main/java/org/kevinvelasquez/controller/InicioController.java
+++ src/main/java/org/kevinvelasquez/controller/InicioController.java
@@ -1,4 +1,7 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
 package org.kevinvelasquez.controller;
 
 import java.net.URL;
@@ -8,8 +11,9 @@ import javafx.fxml.Initializable;
 import org.kevinvelasquez.system.Main;
 
 /**
+ * FXML Controller class
  *
- * @author Kevin
+ * @author informatica
  */
 public class InicioController implements Initializable {
     private Main principal;
@@ -37,5 +41,10 @@ public class InicioController implements Initializable {
     @FXML
     private void vistaProducts() {
         principal.escenaProductos();
-    }    
+    }
+    
+    @FXML
+    private void escenaMenuPrincipal() {
+        principal.menu();
+    }
 }
