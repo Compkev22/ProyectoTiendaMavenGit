ee9d2d8ba94481f39ddc9fc35852a30fb67503f6
Compkev22 <kvelasquez-2022473@kinal.edu.gt>
2025-07-14 23:48:53 -0600

    Commit Cincuenta y Cuatro: Se creo y se construyo el controller de Registrarse

---
+++ src/main/java/org/kevinvelasquez/controller/RegistrarController.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.kevinvelasquez.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import org.kevinvelasquez.system.Main;
+
+/**
+ *
+ * @author Kevin
+ */
+public class RegistrarController implements Initializable {
+
+    private Main principal;
+
+
+    public void setPrincipal(Main principal) {
+        this.principal = principal;
+    }
+
+    public Main getPrincipal() {
+        return principal;
+    }
+
+    public void escenaVolverLogin() {
+        principal.escenaLogin();
+    }
+    
+    public void escenaRegistrarseEntidades() {
+        principal.escenaProductos();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+       
+}
